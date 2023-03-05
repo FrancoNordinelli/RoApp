ee48307c3ac1103c69d0ffe74aaadaeee7615adb
FrancoNordinelli <imnordi@gmail.com>
2023-03-05 15:06:33 -0300

    Nueva clase Oraciones y array de posiciones.

---
+++ app/src/main/java/com/example/rosarioapp/Oraciones.java
@@ -0,0 +1,14 @@
+package com.example.rosarioapp;
+
+public class Oraciones {
+
+    int posX;
+    int posY;
+
+    public Oraciones(int x, int y){
+        this.posX = x;
+        this.posY = y;
+    }
+
+    
+}
